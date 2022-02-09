773b257c51a7d4c5c2a1bfb8dc32d5bbb0efdfb5
pedroc777 <73049669+pedroc777@users.noreply.github.com>
2022-02-08 18:14:29 -0600

    Update Principal.java

--- src/uam/pvoe/archivos2/principal/Principal.java
+++ src/uam/pvoe/archivos2/principal/Principal.java
@@ -22,67 +22,118 @@ public class Principal {
      */
     public static void main(String [] args) {
         Alumno a = new Alumno();
-        LinkedList lista = new LinkedList();
+        LinkedList<Alumno> lista1 = new LinkedList<Alumno>();
         Scanner sc = new Scanner(System.in);
         
-        a = procesarArchivo();
+        lista1 = procesarArchivo();
+       
+        System.out.println("Pick one of the next options:\n");
+        System.out.println("1. Look for a student by registration number\n");
+        System.out.println("2. Look for a student by school name\n");
+        System.out.println("3. Look for a student by score\n");
+        System.out.println("4. Delete a student using registration number\n");
+        System.out.println("5. Exit\n\n");
+        int n = sc.nextInt();
         
-        lista = insertarAlumno(lista, a);
-        //imprimirLista(lista);
-        
-        System.out.println("1. Buscar un alumno por matrícula");
-        System.out.println("2. Buscar alumnos por escuela");
-        System.out.println("3. Buscar alumnos por puntaje superior");
-        System.out.println("4. Borrar un alumno por matrícula");
-        int opcion = sc.nextInt();
-        
-        if(opcion == 1){
-            System.out.println("Dame la matrícula");
-            String m = sc.nextLine();
-            buscarAlumnoMat(lista, m);
+        Scanner zc = new Scanner(System.in);
+        if(n == 1){
+            System.out.println("Write the registration number\n");
+            String camp = zc.nextLine();
+            mostrarAlumno(lista1, camp);
         }
-        else if(opcion == 2){
-            System.out.println("Dame la Escuela");
-            String p = sc.nextLine();
-            buscarAlumnoEsc(lista, p);
+        else if(n == 2){
+            System.out.println("Write the school name\n");
+            String camp = zc.nextLine();
+            mostrarAlumnoEsc(lista1, camp);
         }
-        
+        else if(n == 3){
+            System.out.println("Write the score\n");
+            int camp = zc.nextInt();
+            mostrarPuntaje(lista1, camp);
+        }
+        else if(n == 4){
+            System.out.println("Write the registration number\n");
+            String camp = zc.nextLine();
+            borrarAlumno(lista1, camp);
+            imprimirLista(lista1);
+        }
+        else if(n == 5){
+            System.out.println("Thanks for using this program. Come back soon!");
+        }
     }
     
     
     //métodos
-    public static Alumno procesarArchivo(){
-        Alumno a = new Alumno();
+    public static LinkedList procesarArchivo(){
+        LinkedList<Alumno> lista1 = new LinkedList<Alumno>();
         Lectura lectura = new Lectura();
-        a = lectura.leer("alumnos.dat");
-        return a;
+        lista1 = lectura.leer("alumnos.dat");
+        return lista1;
     }
+    
     
     public static LinkedList insertarAlumno(LinkedList lista, Alumno alumno){
         OperacionesLista operaciones = new OperacionesLista();
-       
         lista = operaciones.insertarAlumno(lista, alumno);
         
         return lista;
     }
+    
+    
     public static void imprimirLista(LinkedList lista){
         OperacionesLista operaciones = new OperacionesLista();
         operaciones.imprimirLista(lista);
     }
     
+    
     public static int buscarAlumnoMat(LinkedList lista, String matricula){
         OperacionesLista operaciones = new OperacionesLista();
         int posicion;
-        
         posicion = operaciones.buscarAlumnoMat(lista, matricula);
+        
         return posicion;
     }
     
-    public static int buscarAlumnoEsc(LinkedList lista, String procedencia){
+    
+    public static void mostrarAlumno(LinkedList lista, String matricula){
+        OperacionesLista operaciones = new OperacionesLista();
+        int posicion;
+        posicion = buscarAlumnoMat(lista, matricula);
+        
+        if(posicion < 0){
+            System.out.println("El alumno no existe");
+        }
+        else{
+            Alumno alu = operaciones.regresarAlumno(lista, posicion);
+            System.out.println("Sus datos son: " + alu.toString());
+        }
+    }
+    
+    
+    public static void mostrarAlumnoEsc(LinkedList lista, String procedencia){
+        OperacionesLista operaciones = new OperacionesLista();
+        operaciones.mostrarAlumnoEsc(lista, procedencia);
+    }
+    
+    
+    public static void mostrarPuntaje(LinkedList lista, int puntaje){
+        OperacionesLista operaciones = new OperacionesLista();
+        operaciones.mostrarPuntaje(lista, puntaje);
+    }
+    
+    
+    public static LinkedList borrarAlumno(LinkedList lista, String matricula){
         OperacionesLista operaciones = new OperacionesLista();
         int posicion;
         
-        posicion = operaciones.buscarAlumnoMat(lista, procedencia);
-        return posicion;
+        posicion = buscarAlumnoMat(lista, matricula);
+        
+        if(posicion < 0){
+            System.out.println("No existe el alumno");
+        }
+        else{
+            lista = operaciones.borrarAlumno(lista, posicion);
+        }
+        return lista;
     }
 }
